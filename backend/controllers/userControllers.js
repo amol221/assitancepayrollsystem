14dec2f6ac6e9a7665de2b02add847c9b05074ee
Amolnilkanthe <patilamol1045@gmail.com>
2022-11-03 13:23:52 +0530

    first commit

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,75 @@
+
+const asyncHandler = require('express-async-handler')
+const User = require("../models/userModel");
+const generateToken = require('../utils/generateToken');
+
+
+const registerUser = asyncHandler(async(req,res)=>{
+    const{ name ,email ,password, mobno} =req.body;
+    
+    const userExists =await User.findOne({ email});
+   
+    if(userExists){
+        res.status(400);
+        throw new Error('user already Exists');
+       }
+       
+    const user = await User.create({
+        name,
+        email,
+        password,
+        mobno,
+    });
+       
+       if(user){
+
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+            mobno:user.mobno,
+            token:generateToken(user._id),
+
+        });
+    }
+    
+    else{
+        res.status(400)
+        throw new Error("error Occured");
+    }
+   
+    
+});
+
+
+const authUser = asyncHandler(async(req,res)=>{
+    const{email ,password} =req.body;
+    
+const user = await User.findOne({"email":email});
+
+if(user &&(await user.matchPassword(password))){
+
+    res.json({
+
+        _id:user._id,
+        name:user.name,
+        email:user.email,
+        isAdmin:user.isAdmin,
+        mobno:user.mobno,
+        token:generateToken(user._id),
+
+
+
+    });
+}
+   else{
+    res.status(400)
+    throw new Error("invalid Email Or Password");
+}
+   
+    
+});
+
+
+module.exports={registerUser,authUser}
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv =require('dotenv')
+const connectDB = require("./config/DB");
+const userRoutes=require('./routes/userRoutes');
+const { notFound, errorHandlers } = require('./middlewares/errorMiddleware');
+
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+//app.get('/',(req,res)=>{
+  //  res.send("Api is running..");
+//});
+
+app.use('/api/users',userRoutes);
+
+app.use(notFound);
+app.use(errorHandlers);
+
+
+const PORT=5000;
+
+
+
+app.listen(5000,console.log("server started on PORT 5000"));
